2e0bf38fc521dd2a7d0defb4df52387c6b831031
Chris Quigley <chris.quigley2@gmail.com>
2025-05-06 12:20:17 -0400

    Views

---
+++ cbindings/view.go
@@ -0,0 +1,84 @@
+//go:build cgo
+// +build cgo
+
+package main
+
+/*
+#include "result.h"
+*/
+import "C"
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/lens-vm/lens/host-go/config/model"
+	"github.com/sourcenetwork/defradb/client"
+	"github.com/sourcenetwork/immutable"
+)
+
+//export viewAdd
+func viewAdd(cQuery *C.char, cSDL *C.char, cTransform *C.char) *C.Result {
+	ctx := context.Background()
+	query := C.GoString(cQuery)
+	sdl := C.GoString(cSDL)
+	lensCfgJson := C.GoString(cTransform)
+
+	// Decode the lens configuration into a lens
+	var transform immutable.Option[model.Lens]
+	if lensCfgJson != "" {
+		decoder := json.NewDecoder(strings.NewReader(lensCfgJson))
+		decoder.DisallowUnknownFields()
+		var lensCfg model.Lens
+		if err := decoder.Decode(&lensCfg); err != nil {
+			return returnC(1, fmt.Sprintf(cerrInvalidLensConfig, err), "")
+		}
+		transform = immutable.Some(lensCfg)
+	}
+
+	// Add the view
+	defs, err := globalNode.DB.AddView(ctx, query, sdl, transform)
+	if err != nil {
+		return returnC(1, err.Error(), "")
+	}
+
+	// Marshall into JSON and return
+	jsonBytes, err := json.MarshalIndent(defs, "", "  ")
+	if err != nil {
+		return returnC(1, err.Error(), "")
+	}
+	return returnC(0, "", string(jsonBytes))
+}
+
+//export viewRefresh
+func viewRefresh(cViewName *C.char, cSchemaRoot *C.char, cVersionID *C.char, cGetInactive C.int) *C.Result {
+	ctx := context.Background()
+	name := C.GoString(cViewName)
+	schemaRoot := C.GoString(cSchemaRoot)
+	versionID := C.GoString(cVersionID)
+	getInactive := cGetInactive != 0
+
+	// Parse the input parameters into a CollectionFetchOptions object
+	options := client.CollectionFetchOptions{}
+	if versionID != "" {
+		options.SchemaVersionID = immutable.Some(versionID)
+	}
+	if schemaRoot != "" {
+		options.SchemaRoot = immutable.Some(schemaRoot)
+	}
+	if name != "" {
+		options.Name = immutable.Some(name)
+	}
+	if getInactive {
+		options.IncludeInactive = immutable.Some(getInactive)
+	}
+
+	// Refresh the views and return
+	err := globalNode.DB.RefreshViews(ctx, options)
+	if err != nil {
+		return returnC(1, err.Error(), "")
+	}
+	return returnC(0, "", "")
+}
